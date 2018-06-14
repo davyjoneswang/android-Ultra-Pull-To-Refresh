a44edb6a6f9cf70c793bd944b1c39c2ece98c72c
wangyh <1051522865@qq.com>
2018-06-14 21:52:10 +0800

    update

--- core/src/in/srain/cube/mints/base/TitleBaseFragment.java
+++ core/src/in/srain/cube/mints/base/TitleBaseFragment.java
@@ -1,11 +1,12 @@
 package in.srain.cube.mints.base;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+
 import in.srain.cube.R;
 import in.srain.cube.app.CubeFragment;
 
@@ -21,14 +22,11 @@ public abstract class TitleBaseFragment extends CubeFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        ViewGroup view = (ViewGroup) inflater.inflate(getFrameLayoutId(), null);
-        LinearLayout contentContainer = (LinearLayout) view.findViewById(R.id.cube_mints_content_frame_content);
-
-        mTitleHeaderBar = (TitleHeaderBar) view.findViewById(R.id.cube_mints_content_frame_title_header);
+        View view = inflater.inflate(getFrameLayoutId(), container, false);
+        LinearLayout contentContainer = view.findViewById(R.id.cube_mints_content_frame_content);
+        mTitleHeaderBar = view.findViewById(R.id.cube_mints_content_frame_title_header);
         if (enableDefaultBack()) {
-            mTitleHeaderBar.setLeftOnClickListener(new OnClickListener() {
-
+            mTitleHeaderBar.setLeftOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     onBackPressed();
@@ -39,10 +37,15 @@ public abstract class TitleBaseFragment extends CubeFragment {
         }
 
         mContentContainer = contentContainer;
-        View contentView = createView(inflater, view, savedInstanceState);
+        View contentView = createView(getLayoutInflater(), mContentContainer, savedInstanceState);
         contentView.setLayoutParams(new LinearLayout.LayoutParams(-1, -1));
         contentContainer.addView(contentView);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 
     protected int getFrameLayoutId() {
